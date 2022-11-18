46a7b7750b457f490f634b98bc9a3e07da18f748
aabenjamim <alanebenjamimdossantos@gmail.com>
2022-11-18 16:29:16 -0300

    versão com item nome editável

--- src/components/Usuario.js
+++ src/components/Usuario.js
@@ -1,18 +1,28 @@
 export default function Usuario(){
+
     return(
         <User imagem="assets/img/catanacomics.svg" nomeuser="catanacomics" nome="Catana"/>
     )
 }
 
 function User(props){
+
+    function editar(){
+        let name = prompt('Que nome deseja inserir?')
+        let novo = document.querySelector('.nome-editavel')
+        novo.innerHTML = name
+    }
+
     return(
     <div class="usuario" data-test="user">
         <img src={props.imagem} data-test="profile-image"/>
         <div class="texto" data-test="name">
             <strong>{props.nomeuser}</strong>
-            <span>
-                {props.nome}
-                <ion-icon name="pencil" data-test="edit-name"></ion-icon>
+            <span data-test="edit-name">
+                <p class="nome-editavel">{props.nome}</p>
+                <div onClick={editar}>
+                    <ion-icon name="pencil"></ion-icon>
+                </div>
             </span>
         </div>
     </div>
