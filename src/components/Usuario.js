19d42db1ce57d14b8ed0c5c7c12584024a0f14c0
aabenjamim <alanebenjamimdossantos@gmail.com>
2022-11-18 16:41:23 -0300

    versao com foto de perfil editável

--- src/components/Usuario.js
+++ src/components/Usuario.js
@@ -7,20 +7,26 @@ export default function Usuario(){
 
 function User(props){
 
-    function editar(){
+    function editarNome(){
         let name = prompt('Que nome deseja inserir?')
         let novo = document.querySelector('.nome-editavel')
         novo.innerHTML = name
     }
 
+    function editarImagem(){
+        let img = prompt('Insira o link da nova foto de perfil')
+        let novo = document.getElementById('image-perfil')
+        novo.src = img
+    }
+
     return(
     <div class="usuario" data-test="user">
-        <img src={props.imagem} data-test="profile-image"/>
+        <img id="image-perfil" src={props.imagem} data-test="profile-image" onClick={editarImagem}/>
         <div class="texto" data-test="name">
             <strong>{props.nomeuser}</strong>
             <span data-test="edit-name">
                 <p class="nome-editavel">{props.nome}</p>
-                <div onClick={editar}>
+                <div onClick={editarNome}>
                     <ion-icon name="pencil"></ion-icon>
                 </div>
             </span>
